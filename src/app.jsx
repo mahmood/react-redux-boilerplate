83c7206ad692e0551291576de3b5f339881848a9
Mahmood Zamani <imahmoodzamani@gmail.com>
2016-12-07 00:54:23 +0330

    add store and Provider HOC

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {reduxThunk} from 'redux-thunk';
 
 import Index from './components/index.jsx';
+import reducers from './reducers';
 import Style from './styles/main.scss';
 
+const storeWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
 render (
-    <Index/>,
+    <Provider store={storeWithMiddleware(reducers)}>
+        <Index/>
+    </Provider>,
     document.getElementById('root')
 );
