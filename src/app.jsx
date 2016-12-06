b52df93d7d20a9dc93bb76aad061d69b3d3e9070
Mahmood Zamani <imahmoodzamani@gmail.com>
2016-12-07 01:19:31 +0330

    add react-router

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
 
 import Index from './components/index.jsx';
 import reducers from './reducers';
@@ -11,7 +12,9 @@ const store = createStore(reducers);
 
 render (
     <Provider store={store}>
-        <Index/>
+        <Router history={browserHistory}>
+            <Route path="/" component={Index}/>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
